f27fee8a49979eab68c10036b7349b7456972caa
Keala Lusk <contactkeala@gmail.com>
2015-09-02 18:26:39 -0700

    converting to web application

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express'),
+	app = express(),
+	path = require('path'),
+	views = path.join(process.cwd(), 'views');
+
+app.get('/', function (req, res) {
+	var homepage = path.join(views, index.html); 
+	res.send(homepage);
+});
+
+app.get('/about', function (req, res) {
+	var aboutPath = path.join(views, 'about.html');
+	res.send(aboutPath)
+});
+
+app.listen(3000, function () {
+	console.log('Code like a girl');
+});
