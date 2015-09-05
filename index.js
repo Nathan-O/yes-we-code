9e022b5e9a894e096bcddadc95051c6a86d439f3
Keala Lusk <contactkeala@gmail.com>
2015-09-04 18:29:24 -0700

    Added login, signup, home page, added path&form  to login

--- index.js
+++ index.js
@@ -6,13 +6,18 @@ var express = require('express'),
 app.use("/static", express.static("public"));
 
 app.get('/', function (req, res) {
-	var homepage = path.join(views, 'index.html'); 
-	res.sendFile(homepage);
+	var homePath = path.join(views, 'index.html'); 
+	res.sendFile(homePath);
 });
 
 app.get('/about', function (req, res) {
 	var aboutPath = path.join(views, 'about.html');
-	res.send(aboutPath)
+	res.sendFile(aboutPath)
+});
+
+app.get('/login', function (req, res) {
+	var loginPath = path.join(views, 'login.html');
+	res.sendFile(loginPath);
 });
 
 app.listen(3000, function () {
