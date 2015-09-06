3a0f5d374f7fdb23a7b91b715c28af46a444100a
Keala Lusk <contactkeala@gmail.com>
2015-09-06 14:24:15 -0700

    Added user login path, redirects to JSON obj profile

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express'),
 	app = express(),
-	db = require("./models"),
+	db = require('./models'),
 	path = require('path'),
 	views = path.join(process.cwd(), 'views/'),	
 	bodyParser = require('body-parser'),
@@ -8,9 +8,9 @@ var express = require('express'),
 
 //parse the posted data and cookie data
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(cookieParser("A secret"));
+app.use(cookieParser('A secret'));
 
-app.use("/static", express.static('public'));
+app.use('/static', express.static('public'));
 
 /* Routes */
 
@@ -36,28 +36,46 @@ app.get('/signup', function (req, res) {
 
 /* API endpoints*/
 
+//creates a user session
+app.post(['/login', '/api/sessions'], function (req, res) {
+	var username = req.body.username;
+	var password = req.body.password;
+	var returnUser = {username: username, password: password};
+	db.User.findOne(returnUser, function (err, user) {
+		if (err){console.log('No access for you!');}
+		if (user) {
+			res.cookie('guid', user._id, {signed: true});
+			res.redirect('/api/profile');
+		} else {
+			res.redirect('/login');
+		}
+	});
+});
+
+//signs a user up and takes them to their profile
 app.post(['/signup', '/api/users'], function (req, res) {
 	var username = req.body.username;
 	var password = req.body.password;
 	var newUser = {username: username, password: password};
 	db.User.create(newUser, function (err, user) {
+		if (err){console.log(err);}
 		if (user) {
-	      		res.cookie("guid", user._id, { signed: true });
-	            	res.redirect("/api/profile")
+	      		res.cookie('guid', user._id, {signed: true});
+	            	res.redirect('/api/profile');
 		} else {
-	                res.redirect("/signup");
+	                res.redirect('/signup');
 		}
   });	
 });
 
 app.get('/api/profile', function (req, res) {
-	console.log(req.cookies)
+	console.log(req.cookies);
 	var guid = req.signedCookies.guid;
 	db.User.findOne({_id: guid}, function (err,user) {
 		if (err) { console.log(err);}
 		res.send({
 			request_headers: req.headers,
-			user: user || "Not Found"
+			user: user || 'Not Found'
 		});
 	});
 });
